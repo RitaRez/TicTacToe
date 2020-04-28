135903e4ee70f8a34f6a8ea0e24ed0d3c672cceb
RitaRez <ritarezborges@gmail.com>
2020-04-28 19:25:34 -0300

    ta funcionando

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 window.onload = buildBoard();
 
 function buildBoard(){
- var w;
+    let w;
+    let id = 0;
     for (var i = 0; i < 3; i++) {
         for (var j = 0; j < 3; j++) {
-            $('#r'+i).append('<div id="'+i+j+'" class="col-4"></div>')
-            w = $('#'+i+j).width();
-            $("#"+i+j).css('height', w);
-            $("#"+i+j).css('max-width' , w);
-            $("#"+i+j).css('padding', 0);
-            $("#"+i+j).css('border-color', 'white');
-            $("#"+i+j).css('border-width', 'thin');
-            $("#"+i+j).append('<p id="s'+i+j+'" class="align-middle"></p>');
-            console.log('ok')
-
+            $('#r'+i).append('<div id="'+id+'" class="cell col-4"></div>')
+            w = $('#'+id).width();
+            $("#"+id).css('height', w);
+            $("#"+id).css('max-width' , w);
+            $("#"+id).css('padding', 0);
+            $("#"+id).css('border-color', 'white');
+            $("#"+id).css('border-width', 'thin');
+            $("#"+id).append('<p id="s'+id+'" class="align-middle"></p>');
             if(i < 2 && j < 2)
-                $("#"+i+j).css('border-style', 'none solid solid none');
+                $("#"+id).css('border-style', 'none solid solid none');
             else if(i<2)
-                $("#"+i+j).css('border-style', 'none none solid none');
+                $("#"+id).css('border-style', 'none none solid none');
             else if(j<2)
-                $("#"+i+j).css('border-style', 'none solid none none');
+                $("#"+id).css('border-style', 'none solid none none');
+            id++;    
         }
     }
     $('#board').css('max-width', w*3)
@@ -31,108 +31,115 @@ function buildBoard(){
     $('#game').css('padding', (b*(2/10)/2));
 
 }
+let max = 0;
+var origBoard = []
+var huBoard = [];
+var aiBoard = [];
+const huPlayer = 'O', aiPlayer = 'X';
+const winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
 
-//-2 PLAYERS
-var count = 0;
-function verify (char){
-    for (var i = 0; i < 3; i++) {
-        if(document.getElementById("s"+i+'0').textContent == document.getElementById("s"+i+'1').textContent){
-            if (document.getElementById("s"+i+'0').textContent == document.getElementById("s"+i+'2').textContent)
-                if (document.getElementById("s"+i+'0').textContent == char)
-                    return true;
-        }
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame() {    
+    document.querySelector(".endgame").style.display = "none";
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		cells[i].addEventListener('click', turnClick, false); 
     }
-    for (var i = 0; i < 3; i++) {
-        if(document.getElementById("s"+'0'+i).textContent == document.getElementById("s"+'1'+i).textContent){
-            if (document.getElementById("s"+'0'+i).textContent == document.getElementById("s"+'2'+i).textContent)
-                if (document.getElementById("s"+'0'+i).textContent == char)
-                    return true;
-        }
-    }
-    if(document.getElementById("s"+'0'+'0').textContent == document.getElementById("s"+'1'+'1').textContent){
-        if (document.getElementById("s"+'0'+'0').textContent == document.getElementById("s"+'2'+'2').textContent)
-            if (document.getElementById("s"+'0'+'0').textContent == char)
-                return true;
-    }
-    if(document.getElementById("s"+'2'+'0').textContent == document.getElementById("s"+'1'+'1').textContent){
-        if (document.getElementById("s"+'2'+'0').textContent == document.getElementById("s"+'0'+'2').textContent)
-            if (document.getElementById("s"+'1'+'1').textContent == char)
-                return true;
-    }
-    return false;
+    for (let i = 0; i < 9; i++) origBoard[i] = i;
 }
-function game(i, j){
-    if(document.getElementById("s"+i+j).innerHTML == 'x' || document.getElementById("s"+i+j).innerHTML == 'o')
+function checkWin(board, player){
+    let new_arr = [];
+    board.forEach((el, i)=>{
+        if(el == player) new_arr.push(i)
+    });
+    new_arr.sort()
+    let checker = (arr, target) => target.every(v => arr.includes(v));   
+    let includes = false; 
+    winCombos.forEach(el => {
+        if(checker(new_arr, el)) includes = true;
+    });
+    return includes;
+}
+function checkTie(){
+    let availSpots = emptySquares();
+    if(availSpots.length === 0 && !checkWin(aiBoard) && !checkWin(huBoard)) return true;
+    else return false;
+}
+function turn(obj, player) {
+    let id = parseInt(obj.index)  
+    origBoard[id] = player;
+    aiBoard.push(id);
+    let el = document.getElementById(id);
+    el.innerText = player;
+    console.log(obj.score)
+}
+function turnClick(el){
+    if(aiBoard.includes(parseInt(el.target.id)) || huBoard.includes(parseInt(el.target.id))) 
         return false;
-
-    w = $('#'+i+j).width();
-    if(count % 2 == 0){
-        document.getElementById("s"+i+j).innerHTML = 'x'
-        $("#s"+i+j).css('color', 'white');
-        $("#s"+i+j).css('font-size', w*0.9);
-        $("#s"+i+j).css('text-align', 'center');
-        $("#s"+i+j).css('margin-top', (w*(-0.2)));
-        if(verify('x') == true){
-            return('x');
-
-        }
-    } else{
-        document.getElementById("s"+i+j).innerHTML = 'o'
-        $("#s"+i+j).css('color', 'white');
-        $("#s"+i+j).css('font-size', w*0.9);
-        $("#s"+i+j).css('text-align', 'center');
-        $("#s"+i+j).css('margin-top', (w*(-0.2)));
-        if(verify('o') == true){
-            return 'o';
-        }
-    }
-    count++;
-    console.log('why dou')
-    return false;
+    el.target.innerText = huPlayer;
+    huBoard.push(parseInt(el.target.id));
+    origBoard[parseInt(el.target.id)] = huPlayer;
+    if (!checkWin(origBoard, aiPlayer)&& !checkWin(origBoard, huPlayer) && !checkTie()) {
+        turn(minimax(origBoard, aiPlayer), aiPlayer);
+    }    
 }
+function emptySquares(){
+    let arr = [];
+    for (let i = 0; i < 9; i++) {
+        if(origBoard.includes(i)) arr.push(i);
+    }
+    return arr;
+}
+function minimax(newBoard, player){
+    let availSpots = emptySquares();
+    if (checkWin(newBoard, aiPlayer)) return {score: 10};
+    else if (checkWin(newBoard, huPlayer)) return {score: -10};
+	else if (availSpots.length === 0) return {score: 0};
 
-$('#00').click(function () {
-    resp = game(0,0);
-    if( resp != false)
-        alert( resp+ ' won');
-});
-$('#01').click(function (){
-    resp = game(0,1);
-    if(resp != false)
-        alert( resp+ ' won');
-});
-$('#02').click(function (){
-    resp = game(0,2);
-    if( resp != false)
-        alert( resp+ ' won');
-});
-$('#10').click(function (){
-    resp = game(1,0);
-    if( resp != false)
-        alert( resp+ ' won');
-});
-$('#11').click(function (){
-    resp = game(1,1);
-    if( resp != false)
-        alert( resp+ ' won');
-});
-$('#12').click(function (){
-    resp = game(1,2);
-    if( resp != false)
-        alert( resp+ ' won');
-});
-$('#20').click(function (){
-    resp = game(2,0);
-    if( resp != false)
-        alert( resp+ ' won')
-});
-$('#21').click(function (){
-    resp = game(2,1);
-    if( resp != false)
-        alert( resp+ ' won')
-});
-$('#22').click(function (){
-    resp = game(2,2);
-    if( resp != false)
-        alert( resp+ ' won')
-});
+    var moves = [];
+	for (var i = 0; i < availSpots.length; i++) {
+		var move = {};
+		move.index = newBoard[availSpots[i]];
+		newBoard[availSpots[i]] = player;
+		if (player == aiPlayer) {
+			var result =  minimax(newBoard, huPlayer);
+			move.score = result.score;
+		} else {
+			var result = minimax(newBoard, aiPlayer);
+			move.score = result.score;
+		}
+		newBoard[availSpots[i]] = move.index;
+		moves.push(move);
+    }
+ 
+    let bestMove;
+	if(player === aiPlayer) {
+		var bestScore = -10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score > bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	} else {
+		var bestScore = 10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score < bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+    return moves[bestMove];
+}
